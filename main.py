b653d5c9870bcdd1b0b0f489f523bfcf7cbc9d6b
KieranOlney <kieran.olney@outlook.com>
2022-05-06 17:15:24 +0100

    Added check for death function

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-import llist
+from llist import dllist, dllistnode
 import bunny
 
+def check_for_death(bunny_list):
+    for bunny in bunny_list:
+        if bunny.is_alive == False:
+            bunny_list.remove(bunny)
+
+def reproduction():
+    pass
+
+def culling():
+    pass
+
+def main():
+    bunny_list = dllist()
+    pass
